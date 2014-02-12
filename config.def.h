3674310c06d9c86ba5a7c0b1ed7014cb73b5e7a9
anthony cantor <ar.cantor@gmail.com>
2014-02-12 17:35:26 +0000

    firefox on tag 2, chromium on tag 1

--- config.def.h
+++ config.def.h
@@ -22,8 +22,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 },
+	{ "Gimp",     NULL,       NULL,       0,            True,        -1 }
 };
 
 /* layout(s) */
