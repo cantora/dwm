d62f6602d2d232f81da4ecc9bc125f2731e5c1da
anthony cantor <ar.cantor@gmail.com>
2014-02-13 14:07:31 +0000

    setup key configs and shiftview function

--- config.def.h
+++ config.def.h
@@ -31,6 +31,8 @@ static const Rule rules[] = {
 static const float mfact      = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster      = 1;    /* number of clients in master area */
 static const Bool resizehints = True; /* True means respect size hints in tiled resizals */
+/* custom functions */
+void shiftview(const Arg *arg);
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -57,29 +59,33 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|Mod1Mask,              XK_space,  spawn,          {.v = dmenucmd } },
+	/*
+	 *{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	 *{ MODKEY,                       XK_b,      togglebar,      {0} },
+	 *{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	 *{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	 *{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	 *{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	 *{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	 *{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	 */
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_q,      killclient,     {0} },
+	/*
+     *{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	 *{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	 *{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	 *{ MODKEY,                       XK_space,  setlayout,      {0} },
+	 *{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	 *{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	 *{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	 *{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	 *{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	 *{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	 *{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+     */
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -90,6 +96,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY,                       XK_p,      shiftview,      {.i = +1 } },
+	{ MODKEY,                       XK_n,      shiftview,      {.i = -1 } }
 };
 
 /* button definitions */
@@ -109,3 +117,22 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
 
+/** Function to shift the current view to the left/right
+ *
+ * @param: "arg->i" stores the number of tags to shift right (positive value)
+ *          or left (negative value)
+ */
+void
+shiftview(const Arg *arg) {
+	Arg shifted;
+
+	if(arg->i > 0) // left circular shift
+		shifted.ui = (selmon->tagset[selmon->seltags] << arg->i)
+		   | (selmon->tagset[selmon->seltags] >> (LENGTH(tags) - arg->i));
+
+	else // right circular shift
+		shifted.ui = selmon->tagset[selmon->seltags] >> (- arg->i)
+		   | selmon->tagset[selmon->seltags] << (LENGTH(tags) + arg->i);
+
+	view(&shifted);
+}
